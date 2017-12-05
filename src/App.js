aa99780d2127a3bde9f3d0317deef993638251f0
Robert <robert20paul@gmail.com>
2017-12-05 00:36:18 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,371 @@
+import React, { PureComponent } from 'react';
+import './App.css';
+
+const MAX_INTERVAL = 1000;
+const START_SPEED = 50;
+
+export default class App extends PureComponent {
+  constructor(props) {
+    super(props);
+    const arrayA = ['B','A','C','B','A','D','B','A','C','B','A','D'];
+    const arrayB = ['A','B','A','Z','D','C','A','B','A','Z','D','C'];
+    const listA = this.parseArray(arrayA);
+    const listB = this.parseArray(arrayB);
+    const matrix = this.generateMatrix(listA.length, listB.length);
+    this.state = {
+      x: 0,
+      y: 0,
+      LCS: '',
+      isRunning: false,
+      speed: START_SPEED,
+      listA, listB, matrix,
+    }
+
+    this.handleRun = this.handleRun.bind(this);
+    this.handleStop = this.handleStop.bind(this);
+    this.handleClear = this.handleClear.bind(this);
+    this.handleChangeList = this.handleChangeList.bind(this);
+    this.renderInputForm = this.renderInputForm.bind(this);
+
+    this.runAlgorithm = this.runAlgorithm.bind(this);
+    this.backTrack = this.backTrack.bind(this);
+  }
+
+  parseArray(array) {
+    let list = []
+    for (let data of array) {
+      list.push({
+        data,
+        backgroundColor: 'transparent',
+      });
+    }
+    return list;
+  }
+
+  generateMatrix(a, b) {
+    let matrix = [];
+    for (let x = 0; x < b; x++) {
+      matrix[x] = [];
+      for (let y = 0; y < a; y++) {
+        matrix[x].push({ data: 0, backgroundColor: 'transparent' });
+      }
+    }
+    return matrix;
+  }
+
+  backTrack() {
+    const { listA, listB, matrix, isRunning } = this.state;
+    let { x, y, LCS } = this.state;
+
+    if (!isRunning || x < 0 || y < 0) {
+      this.handleStop();
+      return;
+    }
+
+    matrix[x][y].backgroundColor = 'lightblue';
+    listB[x].backgroundColor = 'yellow';
+    listA[y].backgroundColor = 'yellow';
+
+    let blockA = [x-1,y];
+    let blockB = [x,y-1];
+    if (listB[x].data === listA[y].data) {
+      LCS = listB[x].data + LCS;
+      x--;
+      y--;
+    } else if ((x === 0 ? 0: matrix[x-1][y].data) > (y === 0 ? 0 : matrix[x][y-1].data)) {
+      if (x > 0) {
+        matrix[x-1][y].backgroundColor = 'green';
+      }
+      if (y > 0) {
+        matrix[x][y-1].backgroundColor = 'red';
+      }
+      x--;
+    } else {
+      if (x > 0) {
+        matrix[x-1][y].backgroundColor = 'red';
+      }
+      if (y > 0) {
+        matrix[x][y-1].backgroundColor = 'green';
+      }
+      y--;
+    }
+
+    let interval = MAX_INTERVAL - (MAX_INTERVAL * (this.state.speed / 100));
+    this.setState({ listA, listB, matrix, x, y, LCS },
+      () => setTimeout(() => {
+        if (blockA[0] > -1 && blockA[1] > -1
+          && matrix[blockA[0]][blockA[1]].backgroundColor === 'red') {
+            matrix[blockA[0]][blockA[1]].backgroundColor = 'transparent';
+        }
+        if (blockB[0] > -1 && blockB[1] > -1
+          && matrix[blockB[0]][blockB[1]].backgroundColor === 'red') {
+            matrix[blockB[0]][blockB[1]].backgroundColor = 'transparent';
+        }
+        if (x < listB.length-1) {
+          listB[x+1].backgroundColor = 'transparent';
+        }
+        if (y < listA.length-1) {
+          listA[y+1].backgroundColor = 'transparent';
+        }
+        this.setState({ listA, listB, matrix },
+          () => this.backTrack());
+      }, interval)
+    )
+  }
+
+  runAlgorithm() {
+    const { listA, listB, matrix, isRunning, x, y } = this.state;
+
+    if (!isRunning) {
+      this.handleStop();
+      return
+    } else if (y === listA.length) {
+      if (x === listB.length - 1) {
+        this.setState({ y : y-1 }, () => this.backTrack());
+      } else {
+        this.setState({x: x+1, y: 0}, () => this.runAlgorithm());
+      }
+      return;
+    }
+
+    if (listB[x].data === listA[y].data) {
+      if (x > 0 && y > 0) {
+        matrix[x-1][y-1].backgroundColor = 'green';
+        matrix[x][y].data = matrix[x-1][y-1].data+1;
+      } else {
+        matrix[x][y].data = 1;
+      }
+    } else {
+      if (x > 0 && y > 0) {
+        if (matrix[x-1][y].data === matrix[x][y-1].data) {
+          matrix[x-1][y].backgroundColor = 'green';
+          matrix[x][y-1].backgroundColor = 'green';
+          matrix[x][y].data = matrix[x-1][y].data;
+        } else if (matrix[x-1][y].data > matrix[x][y-1].data) {
+          matrix[x-1][y].backgroundColor = 'green';
+          matrix[x][y-1].backgroundColor = 'red';
+          matrix[x][y].data = matrix[x-1][y].data;
+        } else {
+          matrix[x-1][y].backgroundColor = 'red';
+          matrix[x][y-1].backgroundColor = 'green';
+          matrix[x][y].data = matrix[x][y-1].data;
+        }
+      } else if (x > 0) {
+        matrix[x-1][y].backgroundColor = 'green';
+        matrix[x][y].data = matrix[x-1][y].data;
+      } else if (y > 0) {
+        matrix[x][y-1].backgroundColor = 'green';
+        matrix[x][y].data = matrix[x][y-1].data;
+      }
+    }
+
+    matrix[x][y].backgroundColor = 'lightblue';
+    listB[x].backgroundColor = 'yellow';
+    listA[y].backgroundColor = 'yellow';
+
+    let interval = MAX_INTERVAL - (MAX_INTERVAL * (this.state.speed / 100));
+    this.setState({ listA, listB, matrix, y: y+1 },
+      () => setTimeout(() => {
+        listB[x].backgroundColor = 'transparent';
+        listA[y].backgroundColor = 'transparent';
+        if (x > 0 && y > 0) {
+          matrix[x-1][y-1].backgroundColor = 'transparent';
+        }
+        if (x > 0) {
+          matrix[x-1][y].backgroundColor = 'transparent';
+        }
+        if (y > 0) {
+          matrix[x][y-1].backgroundColor = 'transparent';
+        }
+        matrix[x][y].backgroundColor = 'transparent';
+        this.setState({ listA, listB, matrix },
+          () => this.runAlgorithm());
+      }, interval)
+    )
+  }
+
+  handleRun(event) {
+    if (!this.state.isRunning) {
+      this.handleClear();
+      this.setState({
+        matrix: this.generateMatrix(this.state.listA.length, this.state.listB.length),
+        isRunning: true,
+        x: 0,
+        y: 0,
+        LCS: '',
+      }, () => this.runAlgorithm());
+    }
+  }
+
+  handleStop(event) {
+    if (this.state.isRunning) {
+      this.setState({
+        isRunning: false,
+      });
+    }
+  }
+
+  handleClear(event) {
+    if (!this.state.isRunning) {
+      this.setState({
+        matrix: this.generateMatrix(this.state.listA.length, this.state.listB.length)
+      })
+    }
+  }
+
+  handleChangeList(event) {
+    if (this.state.isRunning) {
+      return;
+    }
+    let array = event.target.value;
+    this.setState({
+      [event.target.name]: this.parseArray(array),
+      matrix: event.target.name === 'listA'
+        ? this.generateMatrix(array.length, this.state.listB.length)
+        : this.generateMatrix(this.state.listA.length, array.length)
+    });
+  }
+
+  renderHeader() {
+    return (
+      <div>
+        {
+          "Find the longest common subsequence between two strings."
+        }
+        <p/>
+      </div>
+    )
+  }
+
+  renderInputForm() {
+    return (
+      <form>
+        <label>
+          List A
+          <textarea
+            rows="4"
+            cols="50"
+            name="listA"
+            value={this.state.listA.map((obj) => obj.data).join('')}
+            onChange={this.handleChangeList}
+            disabled={this.state.isRunning}
+          />
+        </label>
+        <p/>
+        <label>
+          List B
+          <textarea
+            rows="4"
+            cols="50"
+            name="listB"
+            value={this.state.listB.map((obj) => obj.data).join('')}
+            onChange={this.handleChangeList}
+            disabled={this.state.isRunning}
+          />
+        </label>
+        <p/>
+        <input type="button" value="run" onClick={this.handleRun} />
+        <input type="button" value="stop" onClick={this.handleStop}
+          style={{ marginLeft: 25 }}
+        />
+        <input type="button" value="clear" onClick={this.handleClear}
+          style={{ marginLeft: 25 }}
+        />
+        <p/>
+        <div>
+          <input
+            type="range"
+            value={this.state.speed}
+            onChange={(event) => this.setState({ speed: event.target.value })}
+          />
+        </div>
+        <p/>
+        <label>
+          Longest common subsequence
+          <textarea
+            rows="4"
+            cols="50"
+            value={this.state.LCS}
+            disabled={true}
+          />
+        </label>
+        <p/>
+      </form>
+    );
+  }
+
+  renderNumberBlock(listObj, x, y) {
+    return (
+      <div
+        key={`${x},${y}`}
+        className="NumberBlock"
+        style={{ backgroundColor: listObj.backgroundColor }}
+      >
+        { listObj.data }
+      </div>
+    );
+  }
+
+  renderListDisplay(list, x, y, delta) {
+    let display = [];
+    for (let listObj of list) {
+      display.push(this.renderNumberBlock(listObj, x, y));
+      x += delta[0];
+      y += delta[1];
+    }
+    return display;
+  }
+
+  renderListDisplays() {
+    return (
+      <div style={{ padding: 20 }}>
+        <div
+          style={styles.flexRow}
+          children={[
+            <div key={'0,0'} className="NumberBlock"/>,
+            ...this.renderListDisplay(this.state.listA, 1, 0, [1,0])
+          ]}
+        />
+        <div style={styles.flexRow}>
+          <div children={this.renderListDisplay(this.state.listB, 0, 1, [0,1])} />
+          <div children={this.renderMatrix(this.state.matrix)} />
+        </div>
+      </div>
+    )
+  }
+
+  renderMatrix(matrix) {
+    let display = [];
+    let x = 1;
+    let y = 1;
+    for (let row of matrix) {
+      display.push(
+        <div
+          key={`matrixRow${y}`}
+          style={styles.flexRow}
+          children={this.renderListDisplay(row, x, y, [1,0])}
+        />
+      );
+      y += 1;
+    }
+    return display;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        { this.renderHeader() }
+        <div style={styles.flexRow}>
+          { this.renderInputForm() }
+          { this.renderListDisplays() }
+        </div>
+      </div>
+    );
+  }
+}
+
+const styles = {
+  flexRow: {
+    display: 'flex',
+  },
+}
